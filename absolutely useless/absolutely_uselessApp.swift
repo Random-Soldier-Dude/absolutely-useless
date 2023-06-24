acc41a48dbcdb33fb1bd2062fbf6d0ac9c8d811c
Yew Rei An <randomsoldierdude@gmail.com>
2023-06-24 11:51:50 +0800

    Initial Commit

---
+++ absolutely useless/absolutely_uselessApp.swift
@@ -0,0 +1,17 @@
+//
+//  absolutely_uselessApp.swift
+//  absolutely useless
+//
+//  Created by Yew Rei An on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct absolutely_uselessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
