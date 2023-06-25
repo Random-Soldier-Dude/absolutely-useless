6753ce4fdb0ec70c1b5f9ceb471b7f1f80ec5087
Aditya <adibalkris@gmail.com>
2023-06-25 14:07:43 +0800

    -fixed background(still need to apply it to full screen) -changed emoji -added text -need to add chat bubble icon for the text

--- absolutely useless/ContentView.swift
+++ absolutely useless/ContentView.swift
@@ -26,18 +26,26 @@ struct ContentView: View {
             .opacity(start ? 0 : 1)
             VStack {
                 ZStack {
-                    Image("forest bg.webp")
+                    Image("forest bg")
                         .resizable()
                         .font(.system(size:300))
-                    Text("🙋‍♂️")
+                    Text("🧞‍♂️")
                         .font(.system(size: 300))
-                    Text("👋")
-                        .font(.system(size:100))
-                        .offset(x:-130, y:-10)
-                        .rotationEffect(.degrees(45))
+                        .offset(y:60)
+                    Text("Would you like to\n go on a journey?") //Need to put this into a speech bubble icon
+                        .background(.white)
+                        .font(.system(size:25))
+                        .multilineTextAlignment(.center)
+                        .offset(x: 50, y: -170)
+                    
+                    //                    Text("🙋‍♂️")
+                    //                        .font(.system(size: 300))
+                    //                    Text("👋")
+                    //                        .font(.system(size:100))
+                    //                        .offset(x:-130, y:-10)
+                    //                        .rotationEffect(.degrees(45))
                 }
                 .padding()
-                Text("")
             }
             .padding()
             .opacity(start ? 1 : 0)
